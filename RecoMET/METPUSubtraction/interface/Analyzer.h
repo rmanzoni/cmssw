efaf13e2681ef269f2b168d9029522fc6ccd8960
Raphael Friese <raphael.friese@cern.ch>
2016-02-12 18:35:48 +0100

    pull external dependencies

--- RecoMET/METPUSubtraction/interface/Analyzer.h
+++ RecoMET/METPUSubtraction/interface/Analyzer.h
@@ -4,7 +4,7 @@
 #include "FWCore/Framework/interface/EDAnalyzer.h"
 #include "FWCore/Framework/interface/Event.h"
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
-#include "JMEAnalysis/TreeWrapper/interface/TreeWrapper.h"
+#include "RecoMET/METPUSubtraction/interface/TreeWrapper.h"
 
 namespace JME {
   class Analyzer : public edm::EDAnalyzer {
